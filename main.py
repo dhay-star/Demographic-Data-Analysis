ffc62d86583aac9eb65c3319cba12d6250ea2e08
Dhay <dhayamer.k@gmail.com>
2025-07-29 17:59:06 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from demographic_data_analyzer import calculate_demographic_data
+
+def main():
+    result = calculate_demographic_data()
+    print(result)
+
+if __name__ == "__main__":
+    main()
